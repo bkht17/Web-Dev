0e4d39595823abfe6910a83862875ea03d93cede
bkht17 <bahytt05@gmail.com>
2025-02-13 16:01:01 +0500

    lab5-bez-verstek

--- lab4/task2/kaspi_project/src/app/home/home.component.ts
+++ lab4/task2/kaspi_project/src/app/home/home.component.ts
@@ -8,50 +8,16 @@ import { ProductListService } from '../product-list/product-list.service';
 
 @Component({
   selector: 'app-home',
-  imports: [ProductsComponent, ProductListComponent],
+  imports: [ProductListComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   productList: ProductList[] = [];
-  products: Product[] = [];
 
   productListService: ProductListService = inject(ProductListService);
-  productService: ProductsService = inject(ProductsService);
 
   constructor() {
-    this.products = this.productService.getAllProducts();
     this.productList = this.productListService.getAllCategories();
-  }
-
-  loadProducts() {
-    this.products = this.productService.getAllProducts().map((product) => ({
-      ...product,
-      likes:
-        Number(localStorage.getItem(`likes_${product.id}`)) || product.likes,
-    }));
-  }
-
-  removeProduct(productId: number) {
-    this.products = this.products.filter((product) => product.id !== productId);
-    localStorage.removeItem(`liked_${productId}`);
-    localStorage.removeItem(`likes_${productId}`);
-  }
-
-  updateLikes({
-    id,
-    likes,
-    isLiked,
-  }: {
-    id: number;
-    likes: number;
-    isLiked: boolean;
-  }) {
-    const product = this.products.find((p) => p.id === id);
-    if (!product) return;
-
-    product.likes = likes;
-    localStorage.setItem(`likes_${id}`, likes.toString());
-    localStorage.setItem(`liked_${id}`, isLiked.toString());
   }
 }
